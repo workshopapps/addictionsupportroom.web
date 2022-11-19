769c38b6213bc843f9fe9563c858db027d0ee58c
Dikeh Daniel <dikehdaniel2020@gmail.com>
2022-11-19 15:49:27 -0800

    feat/FE-107-Design-a-Responsive-FAQs-page

--- Frontend/src/Routes/CreateRoute.js
+++ Frontend/src/Routes/CreateRoute.js
@@ -3,7 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 // import Home from '../Pages/Home/Home';
 // import About from '../Pages/About/About';
 // import Contact from '../Pages/Contact/Contact';
-// import Faq from '../Pages/Faq/Faq'
+import Faq from '../Pages/Faq/Faq'
+/* import Team from '../Pages/Team/Team'; */
 import Services from '../Pages/Services/Services';
 import Forum from '../Pages/Forum/Forum';
 
@@ -14,7 +15,8 @@ const CreateRoute = () => {
       {/* <Route path='/' element={<Home/>} /> */}
       {/* <Route path='about' element={<About/>} /> */}
       {/* <Route path='contact' element={<Contact/>} /> */}
-      {/* <Route path='faq' element={<Faq/>} /> */}
+      <Route path='faq' element={<Faq/>} />
+      {/* <Route path='team' element={<Team/>} /> */}
       <Route path='forum' element={<Forum />} />
       <Route path='services' element={<Services />} />
       {/* <Route path="*" element={<NotFound />} /> */}
