97535ecd29c6faf7eedd33b3a889b13c73b564a7
franktunde <stfrankiesjames76@gmail.com>
2022-11-19 17:22:20 +0100

    feat/(FE-86%2C85%2C84%2C83%2C82%2C81%2C80%2C79%2C78%2C77%2C76)-About-Page

--- Frontend/src/Routes/CreateRoute.js
+++ Frontend/src/Routes/CreateRoute.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 // import Home from '../Pages/Home/Home';
-// import About from '../Pages/About/About';
+import About from '../Pages/About/About';
 import Contact from '../Pages/Contact/Contact';
-// import Faq from '../Pages/Faq/Faq'
+import Faq from '../Pages/Faq/Faq'
 import Services from '../Pages/Services/Services';
 import Forum from '../Pages/Forum/Forum';
 
@@ -12,9 +12,9 @@ const CreateRoute = () => {
   return (
     <Routes>
       {/* <Route path='/' element={<Home/>} /> */}
-      {/* <Route path='about' element={<About/>} /> */}
+      <Route path='about' element={<About/>} />
       <Route path='contact' element={<Contact/>} />
-      {/* <Route path='faq' element={<Faq/>} /> */}
+      <Route path='faq' element={<Faq/>} />
       <Route path='forum' element={<Forum />} />
       <Route path='services' element={<Services />} />
       {/* <Route path="*" element={<NotFound />} /> */}
