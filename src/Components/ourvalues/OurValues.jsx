7afb1d830292ada0ecc64fa7021d8d733be5b786
codak77 <seanchi77@gmail.com>
2022-11-20 12:03:11 +0100

    cicd

---
+++ src/Components/ourvalues/OurValues.jsx
@@ -0,0 +1,29 @@
+import "./ourvalues.scss";
+import { BiUser } from "react-icons/bi";
+import ValueData from "../../Data/ValueData";
+
+const OurValues = ({prg1, heading, prg2, value}) => {
+  return (
+    <div className="value">
+      <header>
+        <p>{prg1}</p>
+        <h2>{heading}</h2>
+        <p className="header__text">{prg2}</p> 
+      </header>
+      <section>
+        {value.map((data) => (
+          <div className="value__content">
+            <div className="value__icon">{data.icon}</div>
+            <p className="value__h1">{data.header}</p>
+            <p
+              className="value__p"
+              dangerouslySetInnerHTML={{ __html: data.content }}
+            ></p>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default OurValues;
