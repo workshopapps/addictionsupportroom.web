53da20e17844862101b36dbfaf826c422bd9ecc8
Ella <ellalearnscopy@gmail.com>
2022-11-22 10:49:08 +0100

    fix: resolved merge conflicts

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -8,7 +8,9 @@ from api.example.views import router as example_router
 from api.progress.views import router as progress_router
 from api.example.views import router as example_router
 from api.quote.views import router as quote_router
+
 api_router = APIRouter()
+
 # api_router.include_router(system_router, prefix="/system", tags=["system"])
 api_router.include_router(example_router, prefix="/example", tags=["example"])
 api_router.include_router(contact_router, prefix="/contact")
