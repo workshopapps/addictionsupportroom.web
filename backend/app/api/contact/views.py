cd42549b840c0a00c8b58873b91c8681e8858b01
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2022-11-22 14:17:01 +0300

    feat: add email function to contact us

--- backend/app/api/contact/views.py
+++ backend/app/api/contact/views.py
@@ -4,9 +4,14 @@ from sqlalchemy.orm import Session
 from api import deps
 from db.models import Messages
 
+from email.message import EmailMessage
+import ssl
+import smtplib
+
 router = APIRouter()
 
-
+APP_PASSWORD = "vwpoedpavesodepe"
+TEAM_EMAIL = "crankshaft3182@gmail.com"
 class Contact(BaseModel):
     name: str
     email: str
@@ -26,6 +31,27 @@ def contact(
         db.commit()
     except Exception:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Couldn't add message")
+
+    # Send contact message to team email
+
+    subject = f"Contact Message from {data.name}"
+    body = f"""
+    From: {data.email}
+    
+    {data.message}
+    """
+
+    em = EmailMessage()
+    em["From"] = data.email
+    em["To"] = TEAM_EMAIL
+    em["Subject"] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+        smtp.login(TEAM_EMAIL, APP_PASSWORD)
+        smtp.sendmail(TEAM_EMAIL, TEAM_EMAIL, em.as_string())
 
     return {
         "msg": "Message received"
