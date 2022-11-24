427d373574dcec4e85828b05d2b80f165b4ff563
muhammod1 <muhammodholawale@gmail.com>
2022-11-24 18:01:29 +0100

    Bug(FE-128):Fixed conflicts on Forum page

--- src/Pages/Forum/Forum.jsx
+++ src/Pages/Forum/Forum.jsx
@@ -1,32 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Download from '../../Components/Download/Download'
 import Community from '../../Components/Froum/Community'
 import HashTag from '../../Components/Froum/HashTag'
 import PostFeed from '../../Components/Froum/PostFeed'
 import PostInput from '../../Components/Froum/PostInput'
-import { ForumHash, ForumCommunity, postFeed, posts } from "../../Data/Forum";
+import { ForumHash, OurWivesposts, Beginnerposts, ForumCommunity, postFeed, posts, ImFree, Thinking } from "../../Data/Forum";
 
 const Forum = () => {
+  const [active, setActive ] = useState (posts)
+
   return (
-    <div>
+    <div className="max-w-[1300px] w-[95%] mx-auto">
         <p className='font-[700] text-[40px] text-center mt-[30px] text-blue mb-[20px] '>Forum</p>
-        <section className='block laptop:flex laptop:flex-rol justify-between mx-auto w-[90%]'>
+        <section className='block laptop:flex laptop:flex-rol justify-between mx-auto w-full'>
             <div className=' laptop:w-[30%]'>
                 <HashTag ForumHash={ForumHash}/>
-                <Community ForumCommunity={ForumCommunity} />
+                <Community ForumCommunity={ForumCommunity} active={active} setActive={setActive} />
             </div>
             <div className=' laptop:w-[65%] '>
                 <PostInput postFeed={postFeed} />
-                <PostFeed posts={posts} />
+                {active === posts && <PostFeed posts={posts} /> }
+                <div>
+                  {active === ImFree &&
+                    <div className='mt-10'>
+                      <p className='text-[64px] font-[700]'>No posts yet</p>
+                      <p className='text-[32px] '>Be the first to make a post</p>
+                    </div>
+                  }
+                  {active === Thinking &&
+                    <div className='mt-10'>
+                      <p className='text-[64px] font-[700]'>No posts yet</p>
+                      <p className='text-[32px] '>Be the first to make a post</p>
+                    </div>
+                  }
+                  {active === Beginnerposts && <PostFeed posts={Beginnerposts}/> }
+                  {active === OurWivesposts && <PostFeed posts={OurWivesposts} />}
+
+                </div>
             </div>
         </section>
-        <div className='mx-auto mt-20 mb-10 max-w-[1300px]'>
+        <div className='mx-auto mt-20 mb-10'>
           <Download />
         </div>
         <br />
-        <br />
-    </div>
+        </div>
   )
 }
 
-export default Forum
+export default Forum;
