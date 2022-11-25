8d714eb517d373512fa06d58f7482436b0140701
Seyi <seyipaye@gmail.com>
2022-11-25 05:23:32 +0100

    fix: Notes

--- backend/app/api/home/routers.py
+++ backend/app/api/home/routers.py
@@ -5,8 +5,6 @@ from api.home import schemas, crud
 from db.db import get_db
 from . import quotes
 
-
-
 router = APIRouter()
 
 
@@ -23,42 +21,42 @@ async def post_emotion(request):
     else:
         return (quotes.confused[random.randint(0, 2)])
 
+
 @router.get("/notes/", response_model=list[schemas.ShowNote])
-def get_all_notes(db: Session=Depends(get_db)):
+def get_all_notes(db: Session = Depends(get_db)):
     notes = crud.get_all_notes(db=db)
     return notes
 
 
-
 @router.post("/notes/create")
-def create_note(note: schemas.Note, db: Session=Depends(get_db)):
+def create_note(note: schemas.Note, db: Session = Depends(get_db)):
     db_note = crud.create_note(db=db, note=note)
     return db_note
 
 
-
-@router.get("/notes/today", response_model=list[schemas.ShowNote])
-def get_all_notes_created_today(db: Session=Depends(get_db)):
+@router.get("/notes/today")  #  response_model=list[schemas.ShowNote]
+def get_all_notes_created_today(db: Session = Depends(get_db)):
     notes = crud.get_all_notes_created_today(db=db)
+    print(notes)
     return notes
 
 
-
-@router.get("/notes/{note_id}", response_model=schemas.ShowNote)
-def get_specific_note(note_id: int, db: Session=Depends(get_db)):
+@router.get(
+    "/notes/{note_id}", )  #  response_model=schemas.ShowNote
+def get_specific_note(note_id: int, db: Session = Depends(get_db)):
     note = crud.get_specific_note(db=db, note_id=note_id)
     return note
 
 
-
 @router.delete("/notes/delete/{note_id}")
-def delete_note(note_id: int, db: Session=Depends(get_db)):
+def delete_note(note_id: int, db: Session = Depends(get_db)):
     note = crud.delete_note(db=db, note_id=note_id)
     return note
 
 
-
 @router.put("/notes/update/{note_id}")
-def update_note(note_id: int, note: schemas.Note, db: Session=Depends(get_db)):
+def update_note(note_id: int,
+                note: schemas.Note,
+                db: Session = Depends(get_db)):
     note = crud.update_note(db=db, note_id=note_id, note=note)
     return note
