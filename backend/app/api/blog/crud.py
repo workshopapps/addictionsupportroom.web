a7363b7680d4dd36241af8e7d1c34727e6aa3c27
Ella <ellalearnscopy@gmail.com>
2022-12-03 17:41:09 +0100

    fix/Get blog detail endpoint

--- backend/app/api/blog/crud.py
+++ backend/app/api/blog/crud.py
@@ -5,3 +5,7 @@ from db.models import Blog
 def get_all_blogs(db: Session):
     all_blogs = db.query(Blog).all()
     return all_blogs
+
+def get_detail_blog(db: Session, blog_id: int):
+    blog = db.query(Blog).filter(Blog.id == blog_id).first()
+    return blog
--- backend/app/api/blog/views.py
+++ backend/app/api/blog/views.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from .crud import get_all_blogs
+from .crud import get_all_blogs, get_detail_blog
 from api import deps
 
 router = APIRouter()
@@ -14,3 +14,18 @@ def all_blogs(db: Session = Depends(deps.get_db)):
     '''
     blogs = get_all_blogs(db)
     return blogs
+
+
+@router.get('/blogs/{id}')
+def detail_blog(id: int, db: Session = Depends(deps.get_db)):
+    '''
+    This endpoint is for getting a the blog in the database
+
+    **id** - this field is required
+    '''
+    try: 
+        blog = get_detail_blog(db=db, blog_id=id)
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"there is no blog with id: {id}")
+    return blog
