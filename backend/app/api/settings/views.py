e111fd38e0f1376580253b77c9a70daa0232723f
Ella <ellalearnscopy@gmail.com>
2022-12-09 07:05:53 +0100

    chore: HTTPException-handling code

--- backend/app/api/settings/views.py
+++ backend/app/api/settings/views.py
@@ -9,32 +9,47 @@ from sqlalchemy.orm import Session
 
 router = APIRouter()
 
+
+
 @router.put("/edit/{username}")
-def edit_user_profile(username : str, db: Session = Depends(deps.get_db), user_name : str = None, avatar : str = None):
+def edit_user_profile(request: Request, username : str, db: Session = Depends(deps.get_db), user_name : str = None, avatar : str = None):
+    if request.method == "PUT":
 
-    user = db.query(User).filter(User.username==username).first()
+        user = db.query(User).filter(User.username==username).first()
 
-    old_user_profile = {
-        "username": user.username,
-        "avatar": user.avatar
-    }
+        if not user:
+            return HTTPException(
+                status_code=status.HTTP_204_NO_CONTENT,
+                detail="User not found"
+            )
 
-    if user_name:
-        user.username = user_name
-        db.commit()
-    
-    if avatar:
-        user.avatar = avatar
-        db.commit()
+        old_user_profile = {
+            "username": user.username,
+            "avatar": user.avatar
+        }
+
+        if user_name:
+            user.username = user_name
+            db.commit()
         
-    db.refresh(user)
+        if avatar:
+            user.avatar = avatar
+            db.commit()
+            
+        db.refresh(user)
 
-    new_user_profile = {
-        "username": user.username,
-        "avatar": user.avatar
-    }
+        new_user_profile = {
+            "username": user.username,
+            "avatar": user.avatar
+        }
 
-    return {
-        "old_user_profile": old_user_profile,
-        "new_user_profile": new_user_profile
-    }
+        return {
+            "old_user_profile": old_user_profile,
+            "new_user_profile": new_user_profile
+        }
+    
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
+            detail="Method not allowed"
+        )
